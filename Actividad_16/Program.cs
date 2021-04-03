28af3fd901cbd10782ad069faf889b1d88d62c86
MARIA RENEÉ ROJAS ARCAYNE <88RO94097790@campus.economicas.uba.ar>
2021-04-03 01:52:49 -0300

    Entrega de la actividad 16, a pedido del docente.

---
+++ Actividad_16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_16
+{
+    class Program
+    {
+        //Solicite un ingreso al usuario y determine si puede ser interpretado como un número entero.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor, ingrese un número: ");
+            int ingreso;
+            bool nroentero = int.TryParse(Console.ReadLine(), out ingreso);
+            Console.WriteLine("\r");
+
+            if (nroentero)
+            {
+                Console.WriteLine("El ingreso es interpretado como un valor numérico.");
+            }
+            else
+            {
+                Console.WriteLine("El ingreso no es interpretado como un valor numérico.");
+            }
+
+            Console.WriteLine("Presione cualquier tecla para finalizar.");
+            Console.ReadKey();
+        }
+    }
+}
